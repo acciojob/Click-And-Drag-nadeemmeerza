18aa6caa3eed694bd18ea4ae95febc15c2ab899a
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-19 12:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Your code here.
 let items = document.getElementsByClassName("item");
+
+function allowDrop(ev) {  ev.preventDefault(); }
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
